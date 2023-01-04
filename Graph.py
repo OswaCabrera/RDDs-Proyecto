5e78fd8a3f5e7ed4e0df53bb28781e676fa10b20
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-01-04 13:00:18 -0600

    Actualizo comentarios

--- Graph.py
+++ Graph.py
@@ -1,44 +1,39 @@
-# Python program to print topological sorting of a DAG
-from collections import defaultdict
- 
-# Class to represent a graph
- 
+from collections import defaultdict 
  
 class Graph:
     def __init__(self, vertices):
-        self.graph = defaultdict(list)  # dictionary containing adjacency List
-        self.V = vertices  # No. of vertices
+        #Diccionario que contiene la lista de adyaciencia
+        self.graph = defaultdict(list) 
+        #Número de vértices
+        self.V = vertices 
  
-    # function to add an edge to graph
+    # Función para agregar un grafo
     def addEdge(self, u, v):
         self.graph[u].append(v)
  
-    # A recursive function used by topologicalSort
+    # Función recursiva utilizada por topologicalSort
     def topologicalSortUtil(self, v, visited, stack):
  
-        # Mark the current node as visited.
+        # Marcamos el nodo como visitado
         visited[v] = True
  
-        # Recur for all the vertices adjacent to this vertex
+        # Recorremos todos los vértices que tienen conexión con este
         for i in self.graph[v]:
             if visited[i] == False:
                 self.topologicalSortUtil(i, visited, stack)
  
-        # Push current vertex to stack which stores result
+        #Hacemos push al vértice
         stack.append(v)
  
-    # The function to do Topological Sort. It uses recursive
-    # topologicalSortUtil()
     def topologicalSort(self):
-        # Mark all the vertices as not visited
+        # Iniicamos todos los vertices como no visitados
         visited = [False]*self.V
         stack = []
  
-        # Call the recursive helper function to store Topological
-        # Sort starting from all vertices one by one
+        # Empezamos a recorrer vértice por vértice
         for i in range(self.V):
             if visited[i] == False:
                 self.topologicalSortUtil(i, visited, stack)
  
-        # Print contents of the stack
-        print(stack[::-1])  # return list in reverse order
+        # Imprimimos la pila al revés 
+        print(stack[::-1])  
