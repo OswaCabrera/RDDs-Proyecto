4162b559ad12fef40def120946b1f42402224db4
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-01-06 17:50:19 -0600

    Corrijo el import

--- RDDs_test.py
+++ RDDs_test.py
@@ -2,41 +2,48 @@
 
 import unittest
 import Graph
-import Operaciones
+from Operaciones import *
 
 class TestRDDs(unittest.TestCase):
     def test_operacionMAP(self):
+        print('Caso 1')
         inicio = OperacionInicial([1, 2, 3, 4, 5])
         dobles = OperacionMap(inicio, lambda x: x*2)
         printInfo()
     def test_operacionReduce(self):
+        print('Caso 2')
         lista= OperacionInicial([2,4,6,8,10])
         lista2 = OperacionReduce(lista, lambda a,b: a+b)
         printInfo()
     def test_operacionReduceTupla(self):
-        lista = OperacionInicial([('llave1',5), ('llave1',6), ('llave2', 3), , ('llave2', 8)])
-        lista_reduce = OperacionReduce(lista3, lambda a,b: a+b)
+        print('Caso 3')
+        lista = OperacionInicial([('llave1',5), ('llave1',6), ('llave2', 3), ('llave2', 8)])
+        lista_reduce = OperacionReduce(lista, lambda a,b: a+b)
         printInfo()
     def test_operacionUnion(self):
-         primero = OperacionInicial([1, 2, 3, 4, 5])
-         segundo = OperacionInicial([6, 7, 8, 9, 10])
-         union = OperacionUnion(primero, segundo)
-         printInfo()
+        print('Caso 4')
+        primero = OperacionInicial([1, 2, 3, 4, 5])
+        segundo = OperacionInicial([6, 7, 8, 9, 10])
+        union = OperacionUnion(primero, segundo)
+        printInfo()
     def test_operacionGroup(self):
+        print('Caso 5')
         lista = OperacionInicial([('llave1',2),('llave2',3), ('llave2',4), ('llave1',3)])
         lista_group = OperacionGroup(lista)
         printInfo()
     def test_operacionJoin(self):
-         lista_uno = OperacionInicial([('llave1',2),('llave2',3), ('llave2',4), ('llave1',3)])
-         lista_dos = OperacionInicial([('llave1', 1), ('llave1',2), ('llave2', 3)])
-         lista_join = OperacionJoin(lista_uno, lista_dos)
-         printInfo()
+        print('Caso 6')
+        lista_uno = OperacionInicial([('llave1',2),('llave2',3), ('llave2',4), ('llave1',3)])
+        lista_dos = OperacionInicial([('llave1', 1), ('llave1',2), ('llave2', 3)])
+        lista_join = OperacionJoin(lista_uno, lista_dos)
+        printInfo()
     def test_grafoComplejo(self):
+        print('Caso 7')
         inicio = OperacionInicial([1, 2, 3, 4, 5])
         dobles = OperacionMap(inicio, lambda x: x*2)
         union = OperacionUnion(inicio, dobles)
         dobles = OperacionMap(union, lambda x: x*2)
-        lista_reduce = OperacionReduce(lista3, lambda a,b: a+b)
+        lista_reduce = OperacionReduce(dobles, lambda a,b: a+b)
         printInfo()
 
 if __name__ == '__main__':
